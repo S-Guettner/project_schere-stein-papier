90f2858b47e4fc10a495bdc2a28895c147951c9c
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-02 11:34:22 +0100

    setup tailwind

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const chooseRounds = document.querySelector("#chooseRounds")
+const userChoiceInput = document.querySelector("#userChoiceInput")
+
+
+
+let numberOfRounds
+chooseRounds.addEventListener('change', (e) => {
+    numberOfRounds = parseInt(e.target.value)
+    chooseRounds.style.display="none"
+    console.log(numberOfRounds)
+})
+
+
+console.log(numberOfRounds)/* undefined with no value */
+
+const computerChoiceArray = ["rock","paper","scissors"]
+let computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)]
+console.log(computerChoice)
+
+
+let roundsPlayed
+
+let userChoice = ""
+
+userChoiceInput.addEventListener('click', (e) => {
+    userChoice = e.target.value
+    console.log(userChoice)
+})
